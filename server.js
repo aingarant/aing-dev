abdeea2140d9b0fb2a80174cf6f69794cd5352f7
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2022-11-23 01:39:11 -0500

    update

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 80;
+
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`Hello!`);
+});
+
+app.get("/meet", (req, res) => {
+  res.writeHead(301, {
+    Location: `https://meet.google.com/otj-iqxh-prj`
+  }).end();
+});
