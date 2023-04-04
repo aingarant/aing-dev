8ecbbba7cc23f2ecdc09373a6ce9fcf129dcf275
Aingaran Thirunadarajah <email@aingaran.com>
2023-04-04 12:28:53 -0400

    Added simplii url

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 80;
 
+const simpliiUrl = "https://mbsy.co/6s7JD2";
+
 app.listen(port, () => {
   console.log(`running on port ${port}`);
 });
@@ -13,6 +15,12 @@ app.get("/", (req, res) => {
 app.get("/meet", (req, res) => {
   res.writeHead(301, {
     Location: `https://meet.google.com/zho-qggt-eqz`
+  }).end();
+});
+
+app.get("/r/simplii", (req, res) => {
+  res.writeHead(301, {
+    Location: simpliiUrl
   }).end();
 });
 
